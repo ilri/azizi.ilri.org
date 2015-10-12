dd7615df9e4cfff8cdf800291b7b6d55622ba94a
Jason Rogena <j.rogena@cgiar.org>
2015-10-12 09:31:32 +0300

    Code for importing sysConfig settings
    Reinsert removed code for dumping the sysConfig configuration from
    azizi_config to the Azizi object in JavaScript
    
    Signed-off-by: Jason Rogena <j.rogena@cgiar.org>

--- index.php
+++ index.php
@@ -141,6 +141,7 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+  Azizi.sysConfig = <?php require_once 'azizi_config';  echo json_encode(Config::$sysConfig); ?>;
   Azizi.searchTimoutID = 0;
   $('[name=azizi_search]').focus().live('keyup', function(){
      window.clearTimeout(Azizi.searchTimoutID);
